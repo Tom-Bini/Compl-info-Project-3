29b76f4930b3c71339d30e28ad2b7176cae5d30b
Tom Bini <tom.bini@student.uliege.be>
2024-11-30 12:21:00 +0100

    Ici où tout commence
    Inclusion des fichiers originaux du projet

---
+++ aiagent.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "aiagent.h"
+#include "board.h"
+#include "agent.h"
+
+Agent *createAiAgent(void) 
+{
+    return NULL;
+}
+
+int GetMinimaxScore(Agent *agent, Board b)
+{
+    return 0;
+}
