bbb221c5b2f211c09838c4ed02b3a13085d9c2d5
Tom Bini <tom.bini@student.uliege.be>
2024-12-13 10:48:25 +0100

    getMinimaxScore presque fini

--- aiagent.c
+++ aiagent.c
@@ -2,13 +2,46 @@
 #include "aiagent.h"
 #include "board.h"
 #include "agent.h"
+#include "Dict.h"
+#include "Dict.c"
+#include "LinkedList.h"
 
 Agent *createAiAgent(void) 
 {
-    return NULL;
+    Dict *hashTable = dictCreate(12000); //Création d'une table de hashage de taille 12000 (Recommandation sur Ecampus)
+
+    
+    return agentCreate("Minimax Guy", , noEnd((Agent *)NULL ,(Board)NULL , ),);
 }
 
-int GetMinimaxScore(Agent *agent, Board b)
+int GetMinimaxScore(Agent *agent, Board b) //Fonction renvoyant la valeur de S(b)
 {
-    return 0;
+    int minimaxScore = -1;
+    for(Move i = 0; i < 9; i++){
+        if(boardValidMove(b, i)){
+            if(dictContains(agent->data, (char *)(b))){
+                return *(int *)dictSearch(agent->data, (char *)(b));
+            } else {
+                if(boardIsFull(b) && boardWin(b) == 'E'){ //Cas où le board est full + draw
+
+                    if(minimaxScore < 0){
+                        minimaxScore = 0;
+                    }
+
+                } else if(boardWin(b) != 'E'){ //Cas où la game est lose
+
+                    //On fait rien parce que minimaxScore est déjà égal à -1 dans le pire des cas
+
+                } else { //Cas où la game est pas finie
+                    int temp = -GetMinimaxScore(agent, boardNext(b, i, agent)); //Formule de Minimax
+                    if(temp > minimaxScore){
+                        minimaxScore = temp;
+                    }
+                }
+                
+                dictInsert(agent->data, b, minimaxScore);
+                return minimaxScore;
+            }
+        }
+    }
 }
--- rlagent.c
+++ rlagent.c
@@ -1,19 +1,20 @@
 #include <stdlib.h>
-#include "aiagent.h"
+#include "rlagent.h"
 #include "board.h"
 #include "agent.h"
+#include "Dict.h"
+#include "LinkedList.h"
 
 Agent *createRlAgent(void) {
     return NULL;
 }
 
-void setTrainingModeRlAgent(Agent *agent, bool training) {
+void setTrainingModeRlAgent(Agent *agent, bool training) { //Training = True -> mode apprentissage, Training = False -> mode exploitation
     return;
 }
 
 
-float getMoveScoreRlAgent(Agent *agent, Board b, Move m)
-{
+float getMoveScoreRlAgent(Agent *agent, Board b, Move m) { //Renvoie le score moyen du move en fonction des données déjà accumulées
     return 0.0;
 }
 
